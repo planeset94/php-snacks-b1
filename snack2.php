a5ef0196b9100132252895de161dd44e0ac17407
planeset94 <planeset94@gmail.com>
2021-06-22 22:05:20 +0200

    linked var to $_GET. Used reg.ex to test email.

--- snack2.php
+++ snack2.php
@@ -1,8 +1,21 @@
+<!-- Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
+che mail contenga un punto e una chiocciola e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
+
 <?php
 
+$name = $_GET['name'];
+$mail=$_GET['mail'];
+$age=$_GET['age'];
+$re = '/[A-z]+@[A-z]+.[A-z]+/m';
+if(strlen($name)>3 && preg_match_all($re, $mail) && is_numeric($age) ){
+$result= 'Accesso Riuscito';
 
-
-
+} else{
+    $result= 'Accesso Negato';
+}
 
 
 
@@ -19,10 +32,10 @@
 </head>
 <body>
     
+<h1> <?= $result  ?> </h1>
 
 
 
-    
 </body>
 </html>
 
