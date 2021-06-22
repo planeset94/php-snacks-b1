32f20745b000a94088d9391463017708b4fbd246
planeset94 <planeset94@gmail.com>
2021-06-22 23:26:57 +0200

    problem with array

--- snack3.php
+++ snack3.php
@@ -1,11 +1,65 @@
+<!-- Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 
+e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z -->
+<pre>
 <?php
 
+// $rand_date =  date( "d-m-y", mt_rand(1, time()));
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+for ($i=0; $i < count($posts); $i++) { 
+    
+
+
+
+
+};
 
 
 
 ?>
 
-
+</pre>
 
 
 <!DOCTYPE html>
@@ -17,6 +71,9 @@
     <title>Snack 3</title>
 </head>
 <body>
+
+
+
     
 </body>
 </html>
