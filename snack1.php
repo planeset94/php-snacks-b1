6f618457c4574161ac40295c3ca9fb90ffd0480f
planeset94 <planeset94@gmail.com>
2021-06-22 20:56:10 +0200

    add multidim array & for-cycle to display elemets

--- snack1.php
+++ snack1.php
@@ -2,15 +2,47 @@
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema.
 Olimpia Milano - Cantù | 55-60 -->
 
-
+<pre>
 <?php
+
+$match=[
+
+    [
+        'casa'=> 'Como',
+        'avversario'=> 'Genova',
+        'punti_casa'=> rand(1,15),
+        'punti_avv'=>rand(1,15),
+    ],
+    [
+        'casa'=> 'Milono',
+        'avversario'=> 'Pescara',
+        'punti_casa'=> rand(1,15),
+        'punti_avv'=>rand(1,15),
+    ],
+    [
+        'casa'=> 'Lecce',
+        'avversario'=> 'Roma',
+        'punti_casa'=> rand(1,15),
+        'punti_avv'=>rand(1,15),
+    ],
+    [
+        'casa'=> 'Olgiate',
+        'avversario'=> 'Cantù',
+        'punti_casa'=> rand(1,15),
+        'punti_avv'=>rand(1,15),
+    ],
+
+
+
+
+];
 
 
 
 
 ?>
 
-
+</pre>
 
 
 <!DOCTYPE html>
@@ -23,8 +55,21 @@ Olimpia Milano - Cantù | 55-60 -->
 </head>
 <body>
     
+<?php for ($i=0; $i < count($match); $i++) { 
+
+$coppia=$match[$i]; 
+
+?> 
+<div>
+    <span> <?=  $coppia ['casa'] ." -" ; ?> </span>
+    <span> <?=  $coppia ['avversario'] . " |"; ?> </span>
+    <span> <?=  $coppia ['punti_casa'] . " -"; ?> </span>
+    <span> <?=  $coppia ['punti_avv'] ; ?> </span>
+</div>
 
 
+ <?php } 
+?>
 
 
 
