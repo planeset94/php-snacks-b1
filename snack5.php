3d0b1d517db006e53e46005ef7ed80203acf6a4f
planeset94 <planeset94@gmail.com>
2021-06-23 08:59:19 +0200

    add php file. Explode parag in sub-str. Show as ul

---
+++ snack5.php
@@ -0,0 +1,71 @@
+<!-- Snack 5
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo 
+in tanti paragrafi. Ogni punto un nuovo paragrafo. -->
+
+<?php 
+$string='Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis blanditiis quae possimus. Officia eum quod maxime a animi quaerat esse, soluta ex voluptatem ducimus. Eaque optio quae quisquam numquam iusto.
+Perferendis illum iste illo, expedita ipsum alias ratione! Eius architecto, repellat adipisci velit similique hic impedit pariatur ipsam vero commodi, quis quasi atque molestias molestiae facere. Architecto praesentium sint rerum.
+Velit in minima voluptates molestiae quisquam, perferendis ad voluptatum cumque quidem natus veritatis quae minus laborum numquam sapiente dolorum nisi ipsam cum amet iste esse. Expedita pariatur inventore perferendis aperiam.
+Dicta maxime, libero commodi porro illum dignissimos doloribus? Voluptas reiciendis nobis sapiente, delectus maxime temporibus. Sint corporis, vero aliquid, doloribus ad accusantium voluptates dolor obcaecati et, quia repudiandae qui suscipit.
+Perferendis rerum hic amet ea libero, inventore praesentium necessitatibus minima, magnam quo totam nisi alias cumque unde similique. Dolorem, eos quos animi laborum soluta quas ipsam et nulla quisquam libero.
+Ad asperiores iure doloremque placeat enim, recusandae quidem perspiciatis tenetur aut, at aliquid aspernatur? Nisi aliquid voluptatem accusamus, consequatur alias dicta? Ipsam libero quidem porro ullam dolor quaerat, saepe quis.
+Similique ipsam quae excepturi vel explicabo laudantium exercitationem cum iste repellat beatae, velit expedita cupiditate inventore ad voluptates accusantium facilis rerum nobis nulla vitae eius, laboriosam magni nisi culpa. Perferendis.
+Ab repellat aliquid illo qui eligendi necessitatibus quas vel, minus est quod culpa nemo architecto obcaecati inventore iusto cum cumque maiores asperiores, accusantium tenetur ullam? Qui ipsum fugit odio quas.
+Voluptas magni error atque beatae temporibus. Totam, odio. Mollitia deserunt recusandae excepturi dolor quae debitis sit molestiae, est quod adipisci repudiandae ad hic voluptas deleniti optio ipsum exercitationem minus voluptatibus!
+Voluptatum veniam iste mollitia, ex repudiandae recusandae ullam, tenetur id soluta nesciunt iusto quidem a nulla! Ipsam non consectetur numquam inventore debitis facilis nobis officiis dolores vitae, natus ex deserunt.
+Maxime sed qui alias voluptatum quae eligendi minus, beatae consequuntur dicta quasi laborum ipsam possimus at, fugiat illum amet repellat dolore adipisci recusandae labore. Porro minus at libero debitis adipisci.
+Distinctio sapiente id facere? Modi voluptates consequuntur quis at provident illo ab adipisci, earum nostrum nemo, dolorem, ipsam sapiente? Dignissimos nihil nulla ratione excepturi. Quos excepturi eveniet amet dicta eos.
+Iure, commodi. Ipsum quidem quasi vel tempora vitae sapiente odio voluptates. Explicabo modi fugiat est omnis perferendis accusamus aliquid numquam consequatur dicta. Pariatur distinctio architecto quis omnis illo fugiat ullam!
+Quos explicabo distinctio nisi aliquam, laudantium sapiente maiores ratione. Fugit autem odio cupiditate adipisci sit velit modi expedita. Eos eum sed ducimus blanditiis sunt saepe nam autem beatae dolores consequuntur!
+Consequuntur, neque. Qui, molestiae corporis rem deleniti neque cupiditate animi odio, consectetur officia necessitatibus in quaerat quod repudiandae ipsa at culpa laudantium fuga fugit? Atque doloribus ipsa pariatur quis illum?
+Corrupti itaque accusamus iure quos esse? Quo similique vitae ipsa modi autem at, velit laboriosam numquam. Doloremque quibusdam nam aliquid cumque, quod nostrum corrupti, nulla hic ad consequuntur suscipit fugit!
+Nesciunt sapiente eos quae. Nisi maxime sit obcaecati blanditiis quasi placeat dolores deserunt quaerat ratione. Sed id similique impedit modi commodi corporis! Dolores cumque incidunt recusandae excepturi sapiente, quisquam obcaecati.
+Cumque nulla nisi labore praesentium assumenda velit, unde laborum itaque sunt! Veniam reprehenderit rerum accusantium magni nobis eum officiis quis tenetur iste cum, unde, ipsam fugiat delectus neque. Sapiente, voluptatibus?
+Excepturi esse repellendus dolorum nam harum tenetur reiciendis distinctio velit praesentium exercitationem beatae quo sunt consequatur iusto veniam aliquam aliquid assumenda obcaecati facilis ea, molestias ipsum vero eaque! Corporis, laboriosam.
+Accusantium distinctio dignissimos maxime ea fuga itaque aut quaerat laudantium incidunt, minima unde molestias veritatis placeat voluptatum illo cumque tempora vel, dolore cum? Hic tempore, inventore neque nemo quod deleniti.
+Dolorem sed explicabo perferendis! Nisi quasi repellat magnam, explicabo dolore veniam molestias laborum illo quae obcaecati reiciendis corporis deleniti unde rerum animi iste molestiae inventore adipisci quod. Nesciunt, officia quae?
+Quaerat quisquam corporis nesciunt similique dolore quas dicta delectus eveniet beatae modi quasi voluptatem aliquam, at nam iure voluptas, dolores explicabo enim. Incidunt corporis consequuntur, neque sunt ut necessitatibus iste!
+Commodi, deleniti nam? Impedit sed placeat vitae laudantium eum nisi mollitia a esse sint adipisci, quisquam in aspernatur dolores, atque id necessitatibus optio reprehenderit velit pariatur vel? Minus, quidem dolor?
+Nihil labore ducimus assumenda dignissimos quos laudantium pariatur impedit! Iste, veniam iure rem cum totam animi repudiandae quae. Quia voluptatibus quas odit modi suscipit nemo non libero quibusdam ex iste!
+Veniam sunt, vero perferendis adipisci, enim, consectetur numquam ratione ducimus consequatur illo placeat odit? Delectus voluptatem magnam, id autem alias suscipit hic nostrum nobis beatae omnis aliquid! Nostrum, illo ipsa!
+Sint aspernatur quidem ea eius voluptatum delectus cupiditate dolores odit eaque commodi voluptatibus obcaecati qui natus beatae laboriosam, consectetur doloremque recusandae quasi illo accusamus molestias dolorum distinctio doloribus. Accusamus, praesentium.
+Commodi excepturi velit, ipsum esse fuga alias sequi beatae omnis itaque voluptatem qui quam, corporis aliquid sit maxime accusamus veritatis! Eligendi, saepe. Neque molestiae error, commodi non quam vel aperiam!
+Magni voluptatum iusto dolorum labore libero minus eligendi ipsa sapiente, voluptates fugiat dolore illum ex animi reprehenderit corrupti minima. Quibusdam, necessitatibus. Repudiandae iure ex et quisquam fugiat odit eum dolorem?
+Dicta fugit saepe consequatur aut vero fuga quam quod ipsam veniam repudiandae magni, reprehenderit ex, non commodi, voluptatem iure hic nemo odit cupiditate expedita perspiciatis porro tempore laborum deserunt. Quas?
+Porro atque, in vel a nulla repellat similique ipsa expedita culpa exercitationem sit cum quidem ad amet quasi voluptas quis! At quia animi maiores laborum? Magnam aut sequi laboriosam animi.';
+
+
+$sub_string= explode('.', $string);
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack 5</title>
+</head>
+<body>
+    
+
+<?php  for ($i=0; $i < count($sub_string); $i++) { ?>
+  <ul>
+      <li><?=  $sub_string[$i];   ?> </li>
+  </ul>
+
+
+<?php 
+}  
+?>
+
+
+
+
+</body>
+</html>
+
