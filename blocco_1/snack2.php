f930536e4ff54912bbefa68cffd8551a5e157d18
planeset94 <planeset94@gmail.com>
2021-06-23 17:40:17 +0200

    snack 2: sadded form

--- blocco_1/snack2.php
+++ blocco_1/snack2.php
@@ -24,19 +24,37 @@ $result= 'Accesso Riuscito';
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Snack 2</title>
 </head>
+
 <body>
-    
-<h1> <?= $result  ?> </h1>
+
+    <form action="http://localhost:8888/PHP/esercitazione_2()php-snacks-b1)/php-snacks-b1/blocco_1/snack2.php"
+        method="get">
+        <input type="text" placeholder="Name" name="name">
+        <br>
+        <input type="text" placeholder="Email" name="mail">
+        <br>
+        <input type="number" placeholder="Age" name="age">
+        <br>
+        <input type="submit" name="submit">
+
+    </form>
+
+
+
+
+
+
+    <h1> <?= $result  ?> </h1>
 
 
 
 </body>
+
 </html>
-
-
